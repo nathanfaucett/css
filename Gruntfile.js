614ae56114c23a211941a909df6851524c028ee1
nathanfaucett <nathanfaucett@gmail.com>
2016-03-03 08:12:33 -0600

    updates

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
 
     grunt.initConfig({
-
         jsbeautifier: {
             files: [
                 "Gruntfile.js",
@@ -9,48 +8,15 @@ module.exports = function(grunt) {
                 "test/**/*.js"
             ]
         },
-
         jshint: {
             options: {
-
-                // be ecmascript3 compatible
                 es3: true,
-
-                // warn when defining and never using variables
                 unused: true,
-
-                // assume a node.js environment
-                node: true,
-
-                // force curly braces with conditionals
                 curly: true,
-
-                // prohibit overwriting native object prototypes
-                freeze: true,
-
-                // only allow functions to be used before their declaration
-                latedef: "nofunc",
-
-                // only allow block nesting of depth 2
-                maxdepth: 2,
-
-                // warn about invalid whitespace characters
-                nonbsp: true,
-
-                // warn about undefined variables
-                // undef: false,
-
-                // prohibit usage of == and !=
                 eqeqeq: true,
-
-                // allow == and != for null only
-                eqnull: true,
-
-                // allow expression usage in assignments
                 expr: true,
-
-                // do not suppress warnings about __proto__
-                proto: false
+                eqnull: true,
+                proto: true
             },
             files: [
                 "Gruntfile.js",
@@ -60,7 +26,7 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.loadNpmTasks("grunt-jsbeautifier");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
     grunt.registerTask("default", ["jsbeautifier", "jshint"]);
 };
--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-## css
+rel [![Build Status](https://travis-ci.org/nathanfaucett/rel.svg?branch=master)](https://travis-ci.org/nathanfaucett/rel)
+=======
 
-css helpers functions for node.js and the browser
+relational algebra for the browser and node.js
+
+
+```javascript
+var rel = require("rel");
+
+var adapter = new SomeAdapter();
+
+var users = rel.createTable("users", {
+    id: "number",
+    email: "string"
+});
+
+// creates new from relation using adapter
+users(adapter)
+
+    // creates new select relation from parent
+    .select([
+        [users.attributes.id, ">", 2],
+        [users.attributes.email, "=", "bill@bill.com"]
+    ])
+
+    // compiles relation and runs it
+    .run(function(error, results) {
+        if (error) {
+            // handle error
+        } else {
+            // handle results
+        }
+    });
+
+```
